0c9af4d681847b685cda026d445563c2e2741e8b
Sherman Burwell <shermanburwell3@gmail.com>
2024-06-02 12:58:27 -0500

    Added code to form.js to grab html elements and create an object. Still in testing

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,7 +2,30 @@ const username = document.getElementById('#username');
 const title = document.getElementById('#title');
 const content = document.getElementById('#content');
 
-const submitButton = document.getElementById('#submit-button');
 
-submitButton.addEventListener('click', )
+
+
+const submitButton = document.querySelector('#submit-button');
+
+submitButton.addEventListener('click', function () {
+
+    // Get textContent from inputs
+    const postUsername = document.querySelector('#post-username').textContent;
+    const postTitle = document.querySelector('#post-title').textContent;
+    const postContent = document.querySelector('#post-content').textContent;
+
+    // Create object for inputs
+    const blogPost = {
+        username: postUsername,
+        title: postTitle,
+        content: postContent
+    };
+
+    // VVV For testing purposes only VVV
+    console.log(blogPost);
+    preventDefault();
+    // ^^^ For testing purposes only ^^^
+});
+
+// Store blogPost into local storage
 
