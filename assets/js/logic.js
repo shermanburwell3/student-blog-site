4b32a16847d23cecbafe8ef0c0d8b6b2db255572
Sherman Burwell <shermanburwell3@gmail.com>
2024-06-05 20:53:16 -0500

    Modified code for dark mode button in logic.js until it worked

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,35 +1,71 @@
 // Add light/dark mode logic here
 
-const darkModeButton = document.querySelector('#dark-mode-button')
+const darkModeButton = document.querySelector('#dark-mode-button');
 let state = darkModeButton.getAttribute('data-state');
-state = darkModeButton.setAttribute('data-state', localStorage.getItem('darkMode'))
+state = localStorage.getItem("darkMode");
+console.log(state);
+
+// Get a handle on the entire page
+const elSelect = document.querySelectorAll('*');
 
 
+if (state === 'light') {
+    darkModeButton.setAttribute('data-state', 'light');
+    for (i = 0; i < elSelect.length; i++) {
+        
+        console.log(state);
+        elSelect[i].setAttribute('style', 'background-color: var(--lm-background); color: var(--lm-color);');
+    }
+}
+else if (state === 'dark') {
+    darkModeButton.setAttribute('data-state', 'dark');
+    for (i = 0; i < elSelect.length; i++) {
+        console.log(state);
+        elSelect[i].setAttribute('style', 'background-color: var(--dm-background); color: var(--dm-color);');
+        
+    }
+}
+
+else {
+    darkModeButton.setAttribute('data-state', 'light');
+    for (i = 0; i < elSelect.length; i++) {
+        console.log(state);
+        elSelect[i].setAttribute('style', 'background-color: var(--lm-background); color: var(--lm-color);');
+    }
+}
 
 darkModeButton.addEventListener('click', function() {
 
-    // Get a handle on the entire page
-    const bodyEl = document.querySelectorAll();
+    
 
     // Get state of page being light mode or dark mode
     let state = darkModeButton.getAttribute('data-state');
 
     // Check state and apply style changes
     if (state === 'light') {
-        state = darkModeButton.setAttribute('data-state', 'dark');
-        console.log(state);
-        bodyEl.setAttribute('style', 'background-color: var(--dm-background); color: var(--dm-color);');
+        for (i = 0; i < elSelect.length; i++) {
+            state = darkModeButton.setAttribute('data-state', 'dark');
+            console.log(state);
+            elSelect[i].setAttribute('style', 'background-color: var(--dm-background); color: var(--dm-color);');
+            localStorage.setItem('darkMode', 'dark');
+        }
     }
     else if (state === 'dark') {
-        state = darkModeButton.setAttribute('data-state', 'light');
-        console.log(state);
-        bodyEl.setAttribute('style', 'background-color: var(--lm-background); color: var(--lm-color);');
+        for (i = 0; i < elSelect.length; i++) {
+            state = darkModeButton.setAttribute('data-state', 'light');
+            console.log(state);
+            elSelect[i].setAttribute('style', 'background-color: var(--lm-background); color: var(--lm-color);');
+            localStorage.setItem('darkMode', 'light');
+        }
     }
 
     else {
-        state = darkModeButton.setAttribute('data-state', 'dark');
-        console.log(state);
-        bodyEl.setAttribute('style', 'background-color: var(--dm-background); color: var(--dm-color);');
+        for (i = 0; i < elSelect.length; i++) {
+            state = darkModeButton.setAttribute('data-state', 'dark');
+            console.log(state);
+            elSelect[i].setAttribute('style', 'background-color: var(--dm-background); color: var(--dm-color);');
+            localStorage.setItem('darkMode', 'dark');
+        }
     }
 
 });
